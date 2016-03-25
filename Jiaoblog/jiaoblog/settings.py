0be4354f97ce009857f51f4cf5a562976024a0c0
markwh1te <iamwh1temark@outlook.com>
2016-03-25 20:29:29 +0800

    bootstrap

--- Jiaoblog/jiaoblog/settings.py
+++ Jiaoblog/jiaoblog/settings.py
@@ -101,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static') 
+STATICFILES_DIRS = (
+        MEDIA_ROOT,
+        BASE_DIR,
+        )
+        
+
